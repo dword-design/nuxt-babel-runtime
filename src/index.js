dbdfe433ed73d2f0b459e47271f3c6abc3faa555
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-06-12 19:38:23 +0000

    fix: foo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import { transform } from '@babel/core'
+import vitePluginVueBabel from '@dword-design/vite-plugin-vue-babel'
 import { babel as rollupPluginBabel } from '@rollup/plugin-babel'
-import { parseVueRequest } from '@vitejs/plugin-vue'
-import { parse } from '@vue/compiler-sfc'
 import { runCommand } from 'nuxi'
 import vitePluginBabel from 'vite-plugin-babel'
-import vueSfcDescriptorToString from 'vue-sfc-descriptor-to-string'
 
 export default (command, args) =>
   runCommand(command, [...args, '--no-fork'], {
@@ -18,33 +15,7 @@ export default (command, args) =>
         plugins: [
           {
             enforce: 'pre',
-            transform: async (code, id) => {
-              const query = parseVueRequest(id)
-              if (
-                query.filename.endsWith('.vue') &&
-                query.query.type !== 'style' &&
-                !query.filename.split('/').includes('node_modules')
-              ) {
-                const sfc = parse(code)
-                for (const section of ['scriptSetup', 'script']) {
-                  if (
-                    sfc.descriptor[section] &&
-                    sfc.descriptor[section].lang === undefined
-                  ) {
-                    sfc.descriptor[section].content = await transform(
-                      sfc.descriptor[section].content,
-                      {
-                        filename: query.filename,
-                      },
-                    ).code
-                  }
-                }
-
-                return vueSfcDescriptorToString(sfc.descriptor)
-              }
-
-              return code
-            },
+            ...vitePluginVueBabel(),
           },
           vitePluginBabel(),
         ],
